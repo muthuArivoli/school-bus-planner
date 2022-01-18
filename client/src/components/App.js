9ccd74d55897e14f03826769ef30bfd9eb43b065
Muthu Arivoli <ma381@duke.edu>
2022-01-18 16:56:21 -0500

    add basic login page

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Login'
+import React, {useState} from 'react';
+
+export default class App extends React.Component {
+
+
+  render() {
+    return (
+      <Login/>
+    );
+  }
+
+}
