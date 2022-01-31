fa7debd2a714305e52b35a0f4f8d1aa3bf91d0ed
Ryan Feinberg <rwf8@duke.edu>
2022-01-30 22:45:29 -0500

    half functionality

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -3,6 +3,7 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import Stack from '@mui/material/Stack';
 import Geocode from "react-geocode";
 import { DataGridPro, useGridApiRef } from '@mui/x-data-grid-pro';
+import Button from '@mui/material/Button';
 
 Geocode.setApiKey("AIzaSyB0b7GWpLob05JP7aVeAt9iMjY0FjDv0_o");
 Geocode.setLocationType("ROOFTOP");
@@ -11,6 +12,12 @@ const containerStyle = {
   width: '50%',
   height: '50vh'
 };
+
+const columns = [
+  { field: 'id', width: 50},
+  { field: 'studentname', width: 150},
+  { field: 'address', width: 400},
+];
 
 const addresses = [
   "4610 Club Terrace NE, Atlanta, GA",
@@ -38,28 +45,10 @@ for (var i=0; i<addresses.length; i++) {
     });
 }
 
-async function helper(latitude, longitude) {
+async function CoordsToAddress(latitude, longitude) {
   let g = await Geocode.fromLatLng(latitude, longitude).then(
     (response) => {
-      const address = response.results[0].formatted_address;
-      let city, state, country;
-      for (let i = 0; i < response.results[0].address_components.length; i++) {
-        for (let j = 0; j < response.results[0].address_components[i].types.length; j++) {
-          switch (response.results[0].address_components[i].types[j]) {
-            case "locality":
-              city = response.results[0].address_components[i].long_name;
-              break;
-            case "administrative_area_level_1":
-              state = response.results[0].address_components[i].long_name;
-              break;
-            case "country":
-              country = response.results[0].address_components[i].long_name;
-              break;
-          }
-        }
-      }
-      //console.log(city, state, country);
-      return address;
+      return response.results[0].formatted_address;
     },
     (error) => {
       console.error(error);
@@ -70,46 +59,57 @@ async function helper(latitude, longitude) {
   return data;
 }
 
-const createRandomRow = (m) => {
-  let latitude = m.lat;
-  let longitude = m.lng;
+const addLocationToRoute = async (location) => {
+  let address_noformat = await CoordsToAddress(location.lat, location.lng);
+  let address = address_noformat.replace(/['"]+/g, '');
 
-  let abc = await helper(latitude, longitude);
-
-  return { id: 14, studentname: "temp", address: abc };
+  return { id: 14, studentname: "temp", address: address };
 };
 
-const columns = [
-  { field: 'id', width: 50},
-  { field: 'studentname', width: 150},
-  { field: 'address', width: 250},
-];
-
-const rows = [];
-
-export default function Map() {
-
-  const apiRef = useGridApiRef();
-
+function getCenter(locations) {
   let lattotal = 0;
   let lngtotal = 0;
 
-  for (var j=0; j<locations.length;j++) {
-    lattotal += locations[j].lat
-    lngtotal += locations[j].lng
+  for (var j = 0; j < locations.length; j++) {
+    lattotal += locations[j].lat;
+    lngtotal += locations[j].lng;
   }
 
   let avglat = lattotal / (locations.length);
   let avglng = lngtotal / (locations.length);
 
-  const center = { 
-    lat: avglat, 
+  const center = {
+    lat: avglat,
     lng: avglng
   };
+  return center;
+}
 
-  const handleAddRow = value => () => {
-    //alert(createRandomRow(value));
-    apiRef.current.updateRows([createRandomRow(value)]);
+export default function Map() {
+ const [rows, setRows] = React.useState([]);
+ const [selectedRows, setSelectedRows] = React.useState([]);
+
+  const apiRef = useGridApiRef();
+
+  const center = getCenter(locations);
+
+  const handleAddRow = (value) => {
+    addLocationToRoute(value).then((res) => {
+        let newRows = [...rows, res];
+        setRows(newRows);
+    })
+  };
+
+  const handleDeleteRow = () => {
+    let unselected_rows = [];
+    const rowIds = apiRef.current.getAllRowIds();
+    
+    for (var i=0; i<rowIds.length;i++) {
+      if (!selectedRows.includes(rowIds[i])) {
+        unselected_rows.push(apiRef.current.getRow(rowIds[i]));
+      }
+      setRows(unselected_rows);
+    }
   };
 
   return (
@@ -119,29 +119,38 @@ export default function Map() {
       >
         <GoogleMap id='marker-example' mapContainerStyle={containerStyle} zoom={3} center={center}>
             {locations.map((location) => (
-                <Marker position={location} onClick={handleAddRow(location)}/>
+                <Marker position={location} onClick={() => handleAddRow(location)}/>
               ))
             }
         </GoogleMap>
       </LoadScript>
 
-      <div style={{ height: 400, width: 450 }}>
-        <div style={{ display: 'flex', height: '100%' }}>
-          <div style={{ flexGrow: 1 }}>
-            <DataGridPro
-              apiRef={apiRef}
-              rows={rows}
-              columns={columns}
-              getRowId={(row) => row.id} //set what is used as ID ******MUST BE UNIQUE***********
-              pageSize={5}
-              rowsPerPageOptions={[5]}
-              disableSelectionOnClick
-              density="compact"
-            />
+      <Stack spacing={1} justifyContent="center">
+        <div style={{ height: 400, width: 600 }}>
+          <div style={{ display: 'flex', height: '100%' }}>
+            <div style={{ flexGrow: 1 }}>
+              <DataGridPro
+                apiRef={apiRef}
+                rows={rows}
+                columns={columns}
+                getRowId={(row) => row.address} //set what is used as ID ******MUST BE UNIQUE***********
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                checkboxSelection
+                onSelectionModelChange={(ids) => {
+                  const selectedIDs = new Set(ids);
+                  const selectedRows = rows.filter((row) =>
+                    selectedIDs.has(row.id),
+                  );
+                  setSelectedRows(selectedRows);
+                }}
+                density="compact"
+              />
+            </div>
           </div>
         </div>
-      </div>
-
+        <Button variant="contained" color="primary" onClick={handleDeleteRow}>Remove Selected Locations</Button>
+      </Stack>
     </Stack>
   )
 }
