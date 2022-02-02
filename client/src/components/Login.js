1ef9658b6a9e6b0a928f3f1ccaf1e891a9b26230
Claudia Chapman <claudia.chapman@duke.edu>
2022-02-02 13:30:14 -0500

    env var

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -27,7 +27,7 @@ export default function SignIn() {
       event.preventDefault();
       const data = new FormData(event.currentTarget);
       // eslint-disable-next-line no-console
-      axios.post(REACT_APP_BASE_URL+'/login', {
+      axios.post(process.env.REACT_APP_BASE_URL+'/login', {
         email: data.get('email'),
         password: data.get('password'),
       }).then((res) => {
