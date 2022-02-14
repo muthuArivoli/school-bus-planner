63eea65e09c6a03e5496d1db8460529604b9c830
Muthu Arivoli <ma381@duke.edu>
2022-02-14 02:28:01 -0500

    Update UI components

--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -250,9 +250,6 @@ export default function SignUp(props) {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
           <Typography component="h1" variant="h5">
             Create User
           </Typography>
@@ -334,6 +331,7 @@ export default function SignUp(props) {
                         Student {index + 1}
                     </Typography>
                 </Box>
+                <Grid container spacing={2}>
                     <Grid item xs={12}>
                     <TextField
                         autoFocus
@@ -353,6 +351,7 @@ export default function SignUp(props) {
                         onChange={(e) => handleStudentChange(index, "id", e.target.value)}
                         fullWidth
                     />
+                    </Grid>
                     <Grid item xs={12}>
                     <Autocomplete
                         autoFocus
@@ -376,7 +375,6 @@ export default function SignUp(props) {
                         renderInput={(params) => <TextField {...params} label="Route Name" />}
                     />
                     </Grid>
-                </Grid>
                 <Grid container justifyContent="center">
                 <Grid item xs={1}>
                   <div
@@ -387,6 +385,7 @@ export default function SignUp(props) {
                       Delete
                     </Button>
                   </div>
+                </Grid>
                 </Grid>
                 </Grid>
                 </React.Fragment>
