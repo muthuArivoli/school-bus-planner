64b4ebdf30b748f1eef4f2440de2dc522daaa88e
Ryan Feinberg <rwf8@duke.edu>
2022-02-15 23:59:01 -0500

    TODO: order stops

--- client/src/components/RoutePlanner.js
+++ client/src/components/RoutePlanner.js
@@ -42,7 +42,8 @@ const routeColumns = [
 
 const stopColumns = [
   { field: 'id', hide: true, width: 30},
-  { field: 'name', headerName: "Stop Name", width: 150},
+  { field: 'index', headerName: "Index", editable: true, width: 100},
+  { field: 'name', headerName: "Stop Name", editable: true, width: 150},
 ];
 
 export default function RoutePlanner(props) {
@@ -65,155 +66,57 @@ export default function RoutePlanner(props) {
   const [stops, setStops] = React.useState([]);
   const [stopRows, setStopRows] = React.useState([]);
 
-    let { id } = useParams();
-    let navigate = useNavigate();
+  let { id } = useParams();
+  let navigate = useNavigate();
 
-    //var routeNameDisplay = (selectionModel.length==0 ? (routeInfo.length==0 ? "None" : routeInfo) : selectionModel);
-
-    // load current routes into page
-    React.useEffect(()=>{
-    const fetchData = async() => {
-        const result = await axios.get(
-            process.env.REACT_APP_BASE_URL+`/school/${id}`, {
-              headers: {
-                  Authorization: `Bearer ${localStorage.getItem('token')}`
-              }
-            }
-          );
-        if(result.data.success) {
-            console.log(result.data.school);
-            let newRouteRows = result.data.school.routes.map((value)=>{return {name: value.name, id: value.id, description: value.description}});
-            setRouteRows(newRouteRows);
-        }
-        else{
-            console.log("a");
-            props.setSnackbarMsg(`Route could not be loaded`);
-            props.setShowSnackbar(true);
-            props.setSnackbarSeverity("error");
-            navigate("/routes");
-        }
-    };
-    fetchData();
-    }, [resetRoute]);
-
-    // load all student data for the school into page
-    React.useEffect(()=>{
-        const fetchData = async() => {
-            const result = await axios.get(
-              process.env.REACT_APP_BASE_URL+`/school/${id}`, {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem('token')}`
-                }
-              }
-            );
-            if (result.data.success){
-              console.log(result.data)
-                let newRows = [];
-                for(let i=0; i<result.data.school.students.length; i++){
-                  const studentRes = await axios.get(
-                    process.env.REACT_APP_BASE_URL+`/student/${result.data.school.students[i]}`, {
-                      headers: {
-                          Authorization: `Bearer ${localStorage.getItem('token')}`
-                      }
-                    }
-                  );
-                  if(studentRes.data.success){
-                        const userRes = await axios.get(
-                            process.env.REACT_APP_BASE_URL+`/user/${studentRes.data.student.user_id}`, {
-                            headers: {
-                                Authorization: `Bearer ${localStorage.getItem('token')}`
-                            }
-                            }
-                        );
-                        if(userRes.data.success){
-                            console.log(userRes.data.user);
-                            const g = await Geocode.fromAddress(userRes.data.user.uaddress);
-                            const {lat, lng} = g.results[0].geometry.location;
-                            console.log(studentRes.data);
-                            newRows = [...newRows, {name: studentRes.data.student.name, id: result.data.school.students[i], address: userRes.data.user.uaddress, location: {lat: lat, lng: lng}, route: studentRes.data.student.route_id}]
-                        }
-                        else{
-                            props.setSnackbarMsg(`Route could not be loaded`);
-                            props.setShowSnackbar(true);
-                            props.setSnackbarSeverity("error");
-                            navigate("/routes");
-                        }
-                  }
-                  else{
-                    props.setSnackbarMsg(`School could not be loaded`);
-                    props.setShowSnackbar(true);
-                    props.setSnackbarSeverity("error");
-                    navigate("/schools");
-                  }
-                }
-                setStudents(newRows);
-            }
-            else{
-              console.log("d");
-              props.setSnackbarMsg(`Route could not be loaded`);
-              props.setShowSnackbar(true);
-              props.setSnackbarSeverity("error");
-              navigate("/routes");
-            }
-          };
-          fetchData();
-    }, [resetRoute])
-
-    // load school data into page
-    React.useEffect(()=>{
-      const fetchData = async() => {
-        const result = await axios.get(
+  // load current routes into page
+  React.useEffect(()=>{
+  const fetchData = async() => {
+      const result = await axios.get(
           process.env.REACT_APP_BASE_URL+`/school/${id}`, {
             headers: {
                 Authorization: `Bearer ${localStorage.getItem('token')}`
             }
           }
         );
-        if(result.data.success){
-          const g = await Geocode.fromAddress(result.data.school.address);
-          const {lat, lng} = g.results[0].geometry.location;
-          console.log({lat: lat, lng: lng})
-          setSchoolLocation({lat: lat, lng: lng})
-        }
-        else{
+      if(result.data.success) {
+          console.log(result.data.school);
+          let newRouteRows = result.data.school.routes.map((value)=>{return {name: value.name, id: value.id, description: value.description}});
+          setRouteRows(newRouteRows);
+      }
+      else{
+          console.log("a");
           props.setSnackbarMsg(`Route could not be loaded`);
           props.setShowSnackbar(true);
           props.setSnackbarSeverity("error");
           navigate("/routes");
-        }
       }
-      fetchData();  
-    },[])
+  };
+  fetchData();
+  }, [resetRoute]);
 
-    // load route info into page if came in through route modify (I think?, doesn't work if so)
-    React.useEffect(() => {
-      const fetchStudents = async() => {
-          if(selectionModel.length == 0){
-              setRouteInfo({"name": "", "description": ""});
-              setStudentRows([]);
-              return;
-          }
-          console.log(selectionModel[0]);
-          let response = await axios.get(
-              process.env.REACT_APP_BASE_URL+`/route/${selectionModel[0]}`, {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem('token')}`
-                }
+  // load all student data for the school into page
+  React.useEffect(()=>{
+      const fetchData = async() => {
+          const result = await axios.get(
+            process.env.REACT_APP_BASE_URL+`/school/${id}`, {
+              headers: {
+                  Authorization: `Bearer ${localStorage.getItem('token')}`
               }
-            );
-            if(response.data.success){
-              setRouteInfo({"name": response.data.route.name, "description": response.data.route.description});
+            }
+          );
+          if (result.data.success){
+            console.log(result.data)
               let newRows = [];
-              for(let i=0; i<response.data.route.students.length; i++){
-                  const studentRes = await axios.get(
-                      process.env.REACT_APP_BASE_URL+`/student/${response.data.route.students[i]}`, {
-                      headers: {
-                          Authorization: `Bearer ${localStorage.getItem('token')}`
-                      }
-                      }
-                  );
-                  if(studentRes.data.success){
-                      console.log(studentRes.data);
+              for(let i=0; i<result.data.school.students.length; i++){
+                const studentRes = await axios.get(
+                  process.env.REACT_APP_BASE_URL+`/student/${result.data.school.students[i]}`, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    }
+                  }
+                );
+                if(studentRes.data.success){
                       const userRes = await axios.get(
                           process.env.REACT_APP_BASE_URL+`/user/${studentRes.data.student.user_id}`, {
                           headers: {
@@ -222,34 +125,131 @@ export default function RoutePlanner(props) {
                           }
                       );
                       if(userRes.data.success){
-                          console.log(userRes.data);
-                          newRows = [...newRows, {name: studentRes.data.student.name, id: response.data.route.students[i], address: userRes.data.user.uaddress}]
+                          console.log(userRes.data.user);
+                          const g = await Geocode.fromAddress(userRes.data.user.uaddress);
+                          const {lat, lng} = g.results[0].geometry.location;
+                          console.log(studentRes.data);
+                          newRows = [...newRows, {name: studentRes.data.student.name, id: result.data.school.students[i], address: userRes.data.user.uaddress, location: {lat: lat, lng: lng}, route: studentRes.data.student.route_id}]
                       }
                       else{
-                        console.log("c")
                           props.setSnackbarMsg(`Route could not be loaded`);
                           props.setShowSnackbar(true);
                           props.setSnackbarSeverity("error");
                           navigate("/routes");
                       }
-                  }
-                  else{
-                    console.log("b");
-                      props.setSnackbarMsg(`Route could not be loaded`);
-                      props.setShowSnackbar(true);
-                      props.setSnackbarSeverity("error");
-                      navigate("/routes");
-                  }
-            }
-            console.log(newRows);
-            setStudentRows(newRows);
+                }
+                else{
+                  props.setSnackbarMsg(`School could not be loaded`);
+                  props.setShowSnackbar(true);
+                  props.setSnackbarSeverity("error");
+                  navigate("/schools");
+                }
+              }
+              setStudents(newRows);
           }
-          
+          else{
+            console.log("d");
+            props.setSnackbarMsg(`Route could not be loaded`);
+            props.setShowSnackbar(true);
+            props.setSnackbarSeverity("error");
+            navigate("/routes");
+          }
+        };
+        fetchData();
+  }, [resetRoute])
 
+  // load school data into page
+  React.useEffect(()=>{
+    const fetchData = async() => {
+      const result = await axios.get(
+        process.env.REACT_APP_BASE_URL+`/school/${id}`, {
+          headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        }
+      );
+      if(result.data.success){
+        const g = await Geocode.fromAddress(result.data.school.address);
+        const {lat, lng} = g.results[0].geometry.location;
+        console.log({lat: lat, lng: lng})
+        setSchoolLocation({lat: lat, lng: lng})
       }
+      else{
+        props.setSnackbarMsg(`Route could not be loaded`);
+        props.setShowSnackbar(true);
+        props.setSnackbarSeverity("error");
+        navigate("/routes");
+      }
+    }
+    fetchData();  
+  },[])
 
-      fetchStudents();
-    }, [selectionModel])
+  // load route info into fields when route is clicked
+  React.useEffect(() => {
+    const fetchStudents = async() => {
+        if(selectionModel.length == 0){
+            setStopRows([]);
+            setRouteInfo({"name": "", "description": ""});
+            setStudentRows([]);
+            return;
+        }
+        console.log(selectionModel[0]);
+        let response = await axios.get(
+            process.env.REACT_APP_BASE_URL+`/route/${selectionModel[0]}`, {
+              headers: {
+                  Authorization: `Bearer ${localStorage.getItem('token')}`
+              }
+            }
+          );
+          if(response.data.success){
+            setRouteInfo({"name": response.data.route.name, "description": response.data.route.description});
+            let newRows = [];
+            for(let i=0; i<response.data.route.students.length; i++){
+                const studentRes = await axios.get(
+                    process.env.REACT_APP_BASE_URL+`/student/${response.data.route.students[i]}`, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    }
+                    }
+                );
+                if(studentRes.data.success){
+                    console.log(studentRes.data);
+                    const userRes = await axios.get(
+                        process.env.REACT_APP_BASE_URL+`/user/${studentRes.data.student.user_id}`, {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('token')}`
+                        }
+                        }
+                    );
+                    if(userRes.data.success){
+                        console.log(userRes.data);
+                        newRows = [...newRows, {name: studentRes.data.student.name, id: response.data.route.students[i], address: userRes.data.user.uaddress}]
+                    }
+                    else{
+                      console.log("c")
+                        props.setSnackbarMsg(`Route could not be loaded`);
+                        props.setShowSnackbar(true);
+                        props.setSnackbarSeverity("error");
+                        navigate("/routes");
+                    }
+                }
+                else{
+                  console.log("b");
+                    props.setSnackbarMsg(`Route could not be loaded`);
+                    props.setShowSnackbar(true);
+                    props.setSnackbarSeverity("error");
+                    navigate("/routes");
+                }
+          }
+          console.log(newRows);
+          setStudentRows(newRows);
+        }
+        
+
+    }
+
+    fetchStudents();
+  }, [selectionModel])
 
   // function when snackbar is closed
   const handleClose = (event, reason) => {
@@ -283,23 +283,19 @@ export default function RoutePlanner(props) {
         lat: value.lat(),
         lng: value.lng(),
       };
-      // update table
+
+      // update stopRows
       let stopindex = stopRows.length;
-      let newStopRow = {id: stopindex, name: "Stop "+stopindex};
+      let newStopRow = {id: value.lat() + value.lng(), index: stopindex, name: "Stop "+stopindex, location: loc};
       let newStopRows = [...stopRows, newStopRow];
       setStopRows(newStopRows);
-
-      // update stops state
-      let newStop = {loc: loc}; //add more information to be passed to backend
-      let newStops = [...stops, newStop];
-      setStops(newStops);
     }
   };
 
   // function when stop icon is clicked
   const handleStopClick = (stop, index) => {
-    let newStopRows = stopRows.filter(value => value.id != stop.id)
-    setStopRows(newStopRows)
+    let newStopRows = stopRows.filter(value => value.id != stop.id);
+    setStopRows(newStopRows);
   };
 
   // function when add/update route button is clicked
@@ -330,7 +326,7 @@ export default function RoutePlanner(props) {
                 setSnackbarSeverity('success');
                 setSnackbarMsg('Route successfully created');
             }
-            else{
+            else {
                 setSnackbarOpen(true);
                 setSnackbarSeverity('error');
                 setSnackbarMsg('Failed to create route');
@@ -352,7 +348,7 @@ export default function RoutePlanner(props) {
             if(res.data.success){
                 setRouteInfo({"name": "", "description": ""});
                 setStudentRows([]);
-                setStudentRows([]);
+                setStopRows([]);
                 setSnackbarOpen(true);
                 setSnackbarSeverity('success');
                 setSnackbarMsg('Route successfully updated');
@@ -387,7 +383,10 @@ export default function RoutePlanner(props) {
     if (newToggle.length) {
       setToggleSelection(newToggle);
     }
-    console.log("mode: "+newToggle);
+  };
+
+  const handleStopCellEdit = (row, allRows) => {
+    console.log(row);
   };
 
   return (
@@ -399,7 +398,7 @@ export default function RoutePlanner(props) {
           onChange={handleToggleMode}
         >
           <ToggleButton value="students">Student Mode</ToggleButton>
-          <ToggleButton value="stops">Stops Mode</ToggleButton>
+          <ToggleButton value="stops" disabled={routeInfo["name"].length == 0}>Stops Mode</ToggleButton>
       </ToggleButtonGroup>
       {toggleSelection=="stops" ? <Stack spacing={0} justifyContent="center">
           <Typography variant="h5" align="left">
@@ -415,6 +414,7 @@ export default function RoutePlanner(props) {
                   pageSize={5}
                   rowsPerPageOptions={[5]}
                   density="compact"
+                  onCellEditStop={(row) => handleStopCellEdit(row, stopRows)}
                 />
               </div>
             </div>
@@ -434,8 +434,8 @@ export default function RoutePlanner(props) {
             {students.map((student, index) => (
               <Marker key={index} title={student.address} position={student.location} onClick={() => handleAddressClick(student, index)} label={student.route == null ? "0" : "1"}/> ))
             }
-            {toggleSelection=="stops" ? stops.map((stop, index) => (
-              <Marker key={index} title={"Stop "+index} position={stop.loc} onClick={() => handleStopClick(stop, index)} label={"s"}/>))
+            {toggleSelection=="stops" ? stopRows.map((stop, index) => (
+              <Marker key={index} title={stop.name} position={stop.location} onClick={() => handleStopClick(stop, index)} label={"s"}/>))
                : []}
           </GoogleMap>
         </LoadScript>
@@ -464,7 +464,7 @@ export default function RoutePlanner(props) {
       </Stack>
       <Stack spacing={2.5} justifyContent="center">
         <Typography variant="h5" align="left">
-          Current Route: {selectionModel.length==0 ? (routeInfo["name"].length==0 ? "None" : routeInfo["name"]) : selectionModel}
+          Current Route: {routeInfo["name"].length==0 ? "None" : routeInfo["name"]}
         </Typography>
         <TextField
           fullWidth
